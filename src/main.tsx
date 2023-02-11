659b6280a52c89a895d519355debe520828e75bf
Mark P <mj.poso18@gmail.com>
2023-02-11 17:39:23 +0800

    Divide screen between app panes

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,13 @@ import ReactDOM from 'react-dom/client';
 class App extends React.Component {
   render() {
     return (
-      <div className="bg-gray-500 text-white h-screen w-screen flex items-center justify-center">
-        Hello, world!
+      <div className="h-screen w-screen grid grid-cols-3">
+        <div className="col-span-2 bg-gray-500">
+          <code>input-fields</code>
+        </div>
+        <div className="bg-neutral-500">
+          <code>output-preview</code>
+        </div>
       </div>
     );
   }
