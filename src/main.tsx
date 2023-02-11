be71f57b1929f1a3a4f40ed7699e4ddea4083792
Mark P <mj.poso18@gmail.com>
2023-02-11 17:16:40 +0800

    Setup Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './view/tailwind.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="bg-gray-500 text-white h-screen w-screen flex items-center justify-center">
+        Hello, world!
+      </div>
+    );
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
