bc1f228f8cde7dc7040c727d3288ec9e67de5878
Mark P <mj.poso18@gmail.com>
2023-02-13 17:53:55 +0800

    Enlarge preview pane a bit

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import { Preview } from './view/components/Preview.js';
 class App extends React.Component {
   render() {
     return (
-      <div className="h-screen w-screen grid grid-cols-3">
-        <div className="col-span-2 bg-gray-500">
+      <div className="h-screen w-screen grid grid-cols-[3fr_2fr]">
+        <div className="bg-gray-500">
           <code>input-fields</code>
         </div>
         <Preview />
