9e2321bce0f75bb529d1506fc59e7f1b25890c23
Mark P <mj.poso18@gmail.com>
2023-02-18 00:53:13 +0800

    Add scrollbar styles to input fields

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import './view/scrollbars.css';
 import './view/tailwind.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
--- src/view/components/InputFields.tsx
+++ src/view/components/InputFields.tsx
@@ -5,7 +5,7 @@ import { WorkSection } from './InputFields/WorkSection.js';
 export class InputFields extends React.Component {
   render() {
     return (
-      <form className="overflow-y-scroll flex flex-col gap-4 p-4">
+      <form className="overflow-y-scroll scrollbar scrollbar-thin flex flex-col gap-4 p-4 pt-0">
         {/* <h1 className="font-bold text-2xl">Fields</h1> */}
         {GeneralSection}
         {WorkSection}
