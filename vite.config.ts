dd7e7ba34b424a60d3f296726867a3f79a95f8c0
Mark P <mj.poso18@gmail.com>
2023-02-19 16:10:50 +0800

    Configure Vite to use relative base paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  /**
+   * Use relative public base path for self-contained build
+   * - https://vitejs.dev/guide/static-deploy.html#github-pages
+   * - https://vitejs.dev/config/shared-options.html#base
+   * - https://stackoverflow.com/questions/69744253/vite-build-always-using-static-paths
+   *
+   */
+  base: './',
+});
