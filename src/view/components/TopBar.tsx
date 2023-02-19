97c100555fe52347c8b4d0f76d4ffe5e39d99047
Mark P <mj.poso18@gmail.com>
2023-02-19 15:22:06 +0800

    Add temporary header app name

--- src/view/components/TopBar.tsx
+++ src/view/components/TopBar.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export class TopBar extends React.Component {
   render() {
     return (
-      <div>
-        <code>top-bar</code>
+      <div className="p-4">
+        <h1 className="text-white text-2xl lowercase font-thin tracking-wider">CV Maker</h1>
       </div>
     );
   }
